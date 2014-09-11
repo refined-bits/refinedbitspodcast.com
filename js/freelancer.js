46c9515ae0ec86d1daf9f83f29cbb76e7c20a969
Chris Antes <chris@chrisdoingweb.com>
2014-09-10 20:17:36 -0700

    Creating/fixing pages for episodes. Moving full episode <article> into an include, refactoring layouts for it. Changing post "subtitle" to "description". Using page description for meta tag, if available. Removing contact_me.js.

--- js/freelancer.js
+++ js/freelancer.js
@@ -11,7 +11,6 @@ $(function() {
     $('html, body').stop().animate({
       scrollTop: $($anchor.attr('href')).offset().top
     }, 1000, 'easeOutQuint');
-    event.preventDefault();
   });
 });
 
